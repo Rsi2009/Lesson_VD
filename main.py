fa256e331e582f8e7e89d1498a80fc6b6996aa45
rsi2024 <164152459+rsi2024@users.noreply.github.com>
2024-07-24 21:03:52 +0300

    (form)

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return render_template("card.html") # Внутри () пишем название html-файла в кавычках
+from app import app
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
